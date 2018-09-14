a1f6ce7d6cf7d7baa8ad25a2ee6b5569617d815f
torinberger <beau.berghausen@education.nsw.gov.au>
2018-09-14 22:33:53 +1000

    added removeItem() functionality

--- public/index.js
+++ public/index.js
@@ -1,10 +1,21 @@
 
 window.onload = function () {
 
+  function getHighestKey(array) {
+    var highest = -1;
+    for (var i = 0; i < array.length; i++) {
+      if(array[i].id > highest) {
+        highest = array[i].id;
+      }
+    }
+
+    return highest + 1;
+  }
+
   Vue.component('todo-item', {
-    props: ['title'],
+    props: ['title', 'id', 'remove'],
     template: `
-      <li>{{ title }}</li>
+      <li v-on:click="remove(id)">{{ title }}</li>
     `
   });
 
@@ -17,9 +28,17 @@ window.onload = function () {
     methods: {
       addItem: function () {
         if(this.addItemTitle) {
-          this.todoList.push({id: this.todoList.length, title: this.addItemTitle});
+          this.todoList.push({id: getHighestKey(this.todoList), title: this.addItemTitle});
 
           this.addItemTitle = '';
+        }
+      },
+      removeItem: function (index) {
+        for (var i = 0; i < this.todoList.length; i++) {
+          if(Number(this.todoList[i].id) == Number(index)) {
+            this.todoList.splice(i, 1);
+            return;
+          }
         }
       }
     }
